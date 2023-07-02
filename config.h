13c5e682f3f6ef30460d552dac8ce2114de48d84
Thibault Herve <126357296+th-herve@users.noreply.github.com>
2023-07-02 10:06:25 +0200

    Add tag mask for chrome

--- config.h
+++ config.h
@@ -27,11 +27,12 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
-    { "discord",   NULL,       NULL,       1 << 5,       0,           -1 },
-    { "Spotify",   NULL,       NULL,       1 << 6,       0,           -1 },
+	/* class            instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",           NULL,       NULL,       0,            1,           -1 },
+	{ "firefox",        NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Google-chrome",  NULL,       NULL,       1 << 2,       0,           -1 },
+    { "discord",        NULL,       NULL,       1 << 5,       0,           -1 },
+    { "Spotify",        NULL,       NULL,       1 << 6,       0,           -1 },
 };
 
 /* layout(s) */
