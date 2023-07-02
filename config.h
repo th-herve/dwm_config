951a0d12a2456c30ff8c1d9835b74f00c921d374
Thibault Herve <126357296+th-herve@users.noreply.github.com>
2023-07-02 12:55:52 +0200

    Add patch alt tag decoration, to change the active tag to full circle

--- config.h
+++ config.h
@@ -21,6 +21,7 @@ static const char *colors[][3]      = {
 
 /* tagging */
 static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
+static const char *alttags[] = { "󰪥", "󰪥", "󰪥", "󰪥", "󰪥", "󰪥", "󰪥", "󰪥", "󰪥" };
 
 static const Rule rules[] = {
 	/* xprop(1):
