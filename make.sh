1fdbdb7564d18a6a73d6e89a5ef0af7ee5837d13
Thibault Herve <126357296+th-herve@users.noreply.github.com>
2023-12-08 18:23:09 +0100

    Update make.sh

--- make.sh
+++ make.sh
@@ -3,3 +3,15 @@
 rm -rf ./config.h
 
 make install
+
+desktop=/usr/share/xsessions/dwm.desktop
+
+if [ ! -f "$desktop" ]; then
+    echo "[Desktop Entry]
+Encoding=UTF-8
+Name=Dwm
+Comment=Dynamic window manager
+Exec=/usr/local/bin/dwm
+Icon=dwm
+Type=XSession" > $desktop
+fi
