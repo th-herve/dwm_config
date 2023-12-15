164880792c77431031f54a5e3b82d32dff6bf3cc
Thibault Herve <126357296+th-herve@users.noreply.github.com>
2023-12-15 09:23:43 +0100

    Made run menu case insensitive

--- config.h
+++ config.h
@@ -78,8 +78,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] =
     "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = {
-    "dmenu_run", "-m",      dmenumon, "-fn",    dmenufont, "-nb",     col_gray1,
-    "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray1, NULL};
+    "dmenu_run", "-m",  dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf",
+    col_gray3,   "-sb", col_cyan, "-sf", col_gray1, "-i",  NULL};
 static const char *termcmd[] = {"st", NULL};
 static const char *media_toggle[] = {"playerctl", "--player=spotify",
                                      "play-pause", NULL};
