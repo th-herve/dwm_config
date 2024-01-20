d54ffe2755b9cf4f5ab1a63f2b362b81527279f5
th-herve <thibault.herve1@gmail.com>
2024-01-20 22:07:08 +0100

    Update autostart blocking

--- autostart_blocking.sh
+++ autostart_blocking.sh
@@ -6,4 +6,7 @@ pad_id=$(xinput | awk -F '=|\\[' '/Synaptics/ { print $2}')
 # Get the prop id for pad tap
 tap_id=$(xinput list-props $pad_id | awk -F '\\(|\\)' '/.*Tapping Enabled \(.*/ { print $2}')
 
+natural_scroll_id=$(xinput list-props $pad_id | awk -F '\\(|\\)' '/.*Natural Scrolling Enabled\(.*/ { print $2}')
+
 xinput set-prop $pad_id $tap_id 1
+xinput set-prop $pad_id $natural_scroll_id 1
