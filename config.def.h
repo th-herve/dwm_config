aa93c73f72ec93a7c423785c381a2c44c59073e1
th-herve <thibault.herve1@gmail.com>
2024-06-17 18:40:43 +0200

    Add more window rule

--- config.def.h
+++ config.def.h
@@ -36,11 +36,12 @@ static const Rule rules[] = {
     {"discord",             NULL,           NULL,           1 << 5,     0,          -1},
     {"Spotify",             NULL,           NULL,           1 << 6,     0,          -1},
     {"steam",               NULL,           NULL,           1 << 7,     0,          -1},
-    {"lutris",              NULL,           NULL,           1 << 7,     0,          -1},
+    {"Lutris",              NULL,           NULL,           1 << 7,     0,          -1},
     /*                                                                                          float x,y,w,h           floatborderpx */
-	{"Gimp",                NULL,           NULL,           0,          1,          -1,         50,50,500,500,          -1 },
+	{"Gimp",                NULL,           NULL,           0,          1,          -1,         50,50,500,500,          -1},
     {"gnome-calculator",    NULL,           NULL,           0,          1,          -1,         1569, 20, 349, 492,     -1},
     {"calendar",            NULL,           NULL,           0,          1,          -1,         1628, 20, 290, 230,     -1},
+    { NULL,                 NULL,   "Picture-in-Picture",   -1,         1,          -1,         1327, 712, 591, 366,    -1},
 
 };
 
